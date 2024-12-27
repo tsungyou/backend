c1c63ee4e4ad59c2999af0f692c3358f0beaa761
李宗祐 <tp_mini@lizongyous-Mac-Studio.local>
2024-12-27 16:29:10 +0800

    update

--- server_1.js
+++ server_1.js
@@ -12,7 +12,7 @@ const server = app.listen(PORT, () => {
 const wss = new WebSocketServer({ server });
 
 const pgClient = new Client({
-    user: 'mini',
+    user: 'postgres',
     host: 'localhost',
     database: 'tw',
     password: 'buddyrich134',
@@ -25,10 +25,9 @@ pgClient.connect()
         console.log("Connected to PostgreSQL!");
 
         pgClient.on("notification", (msg) => {
-            console.log("Received notification: ", msg.payload);
             wss.clients.forEach((client) => {
                 if (client.readyState === client.OPEN) {
-                    client.send(`New message in the database: ${msg.payload}`);
+                    client.send(`${msg.payload}`);
                 }
             });
         });
@@ -63,18 +62,16 @@ let counter = 1;  // Initialize the counter to 1
 // Function to insert new data (for testing)
 function insertNewData() {
     // Create the message using the counter value
-    const message = `Message ${counter}`;
-    
-    // Insert the message into the database
-    pgClient.query(`INSERT INTO messages (content) VALUES ($1)`, [message])
+    const message = `${counter}`;
+    const currentTime = new Date().toLocaleString("en-US", { timeZone: "Asia/Taipei" });
+
+    pgClient.query(`INSERT INTO public.block_code3_deatil (code, da, cl) VALUES ($1, $2, $3)`, ["2330", currentTime, 111])
         .then(() => {
             console.log(`Inserted new data: ${message}`);
         })
         .catch(err => console.error("Error inserting data:", err));
 
-    // Increment the counter for the next message
     counter++;
 }
 
-// For demonstration purposes, let's insert new data every 5 seconds
-setInterval(insertNewData, 5000);
+setInterval(insertNewData, 10000);
--- side_server/py_restapi/訊號表.sql
+++ side_server/py_restapi/訊號表.sql
@@ -17,3 +17,29 @@ ALTER TABLE IF EXISTS public.block_code3_deatil
     OWNER to mini;
 
 GRANT ALL ON TABLE public.block_code3_deatil TO mini;
+
+
+
+DROP TABLE IF EXISTS public.block_code3_deatil;
+CREATE TABLE IF NOT EXISTS public.block_code3_deatil
+(
+    code character varying(50) COLLATE pg_catalog."default",
+    da timestamp without time zone NOT NULL,
+    cl double precision
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+CREATE OR REPLACE FUNCTION notify_new_message() 
+RETURNS TRIGGER AS $$
+BEGIN
+  PERFORM pg_notify('new_message_channel', row_to_json(NEW)::text);
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER new_message_trigger
+AFTER INSERT ON block_code3_deatil
+FOR EACH ROW EXECUTE FUNCTION notify_new_message();
